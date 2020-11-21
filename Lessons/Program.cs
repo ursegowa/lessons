8f620eb27c0a9b0eb0834c41dc2690c93fdf4cc9
ursegowa <ursegowa@gmail.com>
2020-11-21 17:43:53 +0400

    Разворот трёх значного числа

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,9 +10,15 @@ namespace Lessons
     {
         static void Main()
         {
-            Console.WriteLine("Введи своё имя: ");
-            var name = Console.ReadLine();
-            Console.WriteLine("Привет, " + name + "!");
+            Console.WriteLine("Введи трёхзначное число: ");
+            var input = Console.ReadLine();
+            var number = 0;
+            Int32.TryParse(input, out number);
+            Console.WriteLine(number);
+            var hundreds = number / 100;
+            var decades = (number / 10) % 10;
+            var units = number % 10;
+            Console.WriteLine("{0}{1}{2}", units, decades, hundreds);
             Console.ReadKey();
         }
     }
