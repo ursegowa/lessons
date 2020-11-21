5e224f9c9d3b87537239466be21f11a4bd5c6f63
ursegowa <ursegowa@gmail.com>
2020-11-21 16:52:03 +0400

    Первая программа

---
+++ Lessons/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введи своё имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name);
+        }
+    }
+}
